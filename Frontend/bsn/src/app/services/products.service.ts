8f1767df937aff023a0875237fc6eac35ea60b27
Azar Ansar <azarmohamed96@yahoo.com>
2023-12-30 18:59:37 -0600

    product service .also,Getting couple of interviews. I will have to pause this.

--- Frontend/bsn/src/app/services/products.service.ts
+++ Frontend/bsn/src/app/services/products.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+const BASE_URL = ''
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
+  private http = inject(HttpClient)
+
   constructor() { }
+
+  getAllProducts(page = 1){
+  }
+
+  getProductsNearby(){
+
+  }
+
 }
