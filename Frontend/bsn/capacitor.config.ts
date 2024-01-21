c445032e7b7e5352ea2da685877d794fe19800b6
Azar Ansar <azarmohamed96@yahoo.com>
2024-01-21 11:44:57 -0600

    unnecessary files deleted

--- Frontend/bsn/capacitor.config.ts
+++ Frontend/bsn/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'bsn',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
